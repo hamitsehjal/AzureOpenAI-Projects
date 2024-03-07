d818e92564d05e3bc8b1e44727c87fa0af3f2be6
Hamit Sehjal <hamitsehjal26@gmail.com>
2024-03-06 20:05:16 -0500

    remove embedding file

--- embedding_test.py
+++
@@ -1,6 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI()
-
-result = client.embeddings.create(model="text-embedding-ada-002", input="Hello world",encoding_format="float")
-print(result.data[0].embedding)
